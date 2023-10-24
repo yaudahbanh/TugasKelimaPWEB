fee8b37cc0230368e9d6d99da8eb5519463e7601
yaudahbanh <juniorvincent32@gmail.com>
2023-10-24 09:13:39 +0700

    feat: update app js

--- ToDoList/app.js
+++ ToDoList/app.js
@@ -2,11 +2,10 @@ const app = Vue.createApp ({
     data() {
         return {
             list: [
-               {nama: 'Bima' },
-               {IPK: '3.81'},
-               {nama: 'Iftala' },
-               {IPK: '3.51'},
+               {nama: 'Bima', ipk: '3.81' },
+               {nama: 'Iftala', ipk: '3.51' }
             ]
         }
     }
 })
+app.mount('#app')
